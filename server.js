3008bc6ceebec7f1b764ba1238417006eec683c8
nicolassss16 <nicomerlini@live.com.ar>
2025-12-21 23:36:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,30 +7,78 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // --- CONFIGURACIÓN DE CATEGORÍAS ---
+// --- CONFIGURACIÓN DE CATEGORÍAS (MODO BESTIA) ---
 const wordPacks = {
     lol: [
         "Teemo", "Yasuo", "Faker", "Baron Nashor", "Flash", "Grieta del Invocador",
-        "Lux", "Darius", "Poro", "Nexos", "Wards", "Yasuo 0/10", "Garen"
-    ],
-    comida: [
-        "Pizza", "Hamburguesa", "Sushi", "Asado", "Empanadas", "Helado",
-        "Chocolate", "Tacos", "Ensalada", "Milanesa", "Pancho", "Mate"
-    ],
-    paises: [
-        "Argentina", "Brasil", "España", "Japón", "Estados Unidos", "Francia",
-        "Italia", "China", "Rusia", "México", "Alemania", "Qatar"
+        "Lux", "Darius", "Poro", "Nexo", "Ward", "Garen", "Thresh", "Lee Sin", 
+        "Smite", "Jinx", "Arcane", "Toplane", "Jungla", "Midlane", "ADC", "Support",
+        "Skin Legendaria", "Pentakill", "Surrender at 15", "T1", "Worlds", 
+        "Blitzcrank", "Shaco", "Yuumi", "Riot Games", "Lag", "Katarina", "Zed",
+        "Ahri", "Vayne", "Maestría 7", "Report", "Trollear", "AFK", "Parche", "Nerf"
     ],
     futbol: [
         "Messi", "Cristiano Ronaldo", "Maradona", "Pelé", "Mbappé", "La Bombonera",
-        "Monumental", "Mundial", "VAR", "Penal", "Tarjeta Roja", "Dibu Martínez"
+        "Monumental", "Mundial", "VAR", "Penal", "Tarjeta Roja", "Dibu Martínez",
+        "Scaloneta", "Julián Álvarez", "Boca Juniors", "River Plate", "Independiente",
+        "Racing", "San Lorenzo", "La 12", "Copa Libertadores", "Champions League", 
+        "Haaland", "Kun Agüero", "Arbitro", "Offside", "Pelota", "Botines", "Arco", 
+        "Corner", "DT", "Hincha", "Barrabrava", "Superclásico", "Gol de Oro"
+    ],
+    comida: [
+        "Pizza", "Hamburguesa", "Sushi", "Asado", "Empanadas", "Helado",
+        "Chocolate", "Tacos", "Ensalada", "Milanesa con Puré", "Pancho", "Mate",
+        "Fernet con Coca", "Dulce de Leche", "Alfajor", "Choripán", "Polenta", 
+        "Guiso de Lentejas", "Vitel Toné", "Mandarina", "Café", "Tostado",
+        "Ñoquis", "Flan con Crema", "Tortilla de Papas", "Facturas", "Chipá",
+        "Coca Cola", "Cerveza", "Papas Fritas", "Fideos con Tuco", "Sanguche de Miga"
+    ],
+    paises: [
+        "Argentina", "Brasil", "España", "Japón", "Estados Unidos", "Francia",
+        "Italia", "China", "Rusia", "México", "Alemania", "Qatar", "Inglaterra",
+        "Uruguay", "Chile", "Colombia", "Perú", "Australia", "Egipto", "Corea del Sur", 
+        "Canadá", "India", "Sudáfrica", "Ucrania", "Portugal", "Suiza", "Grecia"
     ],
     streamers: [
         "Ibai", "AuronPlay", "Rubius", "Spreen", "Davoo Xeneize", "Coscu",
-        "Momo", "Kun Agüero", "TheGrefg", "ElMariana", "Rivers"
+        "Momo", "Kun Agüero", "TheGrefg", "ElMariana", "Rivers", "Bananirou",
+        "Unicornio", "Mazzat", "Luquitas Rodriguez", "Hasbulla", "MrBeast", 
+        "Vegetta777", "Willyrex", "Frankkaster", "La Cobra", "Gastón Edul",
+        "Carrera", "Robleis", "Markito Navaja", "Pellah", "Santutu"
     ],
     cosas: [
-        "Silla", "Mesa", "Computadora", "Celular", "Cama", "Inodoro",
-        "Papel Higiénico", "Espejo", "Ventana", "Puerta", "Televisor"
+        "Silla Gamer", "Mesa", "PC Gamer", "iPhone", "Cama", "Inodoro",
+        "Papel Higiénico", "Espejo", "Ventana", "Puerta", "Televisor",
+        "Auriculares", "Mouse", "Teclado Mecánico", "Zapatillas", "Billetera",
+        "Dólar Blue", "Sube", "Llaves", "Mochila", "Aire Acondicionado", 
+        "Ventilador", "Microondas", "Heladera", "Guitarra", "Pelota de Tenis",
+        "Cargador", "Control Remoto", "Marrón (Color)", "Agua", "Fuego"
+    ],
+    cine: [
+        "Harry Potter", "Star Wars", "El Señor de los Anillos", "Avengers", "Spiderman",
+        "Batman", "Joker", "Titanic", "Shrek", "Toy Story", "Los Simpsons",
+        "Breaking Bad", "Game of Thrones", "El Juego del Calamar", "Casados con Hijos",
+        "Esperando la Carroza", "Los Simuladores", "Relatos Salvajes", "Frozen",
+        "El Padrino", "Matrix", "Stranger Things", "Merlina", "Peaky Blinders"
+    ],
+    marcas: [
+        "Nike", "Adidas", "Apple", "Samsung", "McDonalds", "Burger King",
+        "Coca Cola", "Pepsi", "Manaos", "Marolio", "Ford", "Ferrari",
+        "PlayStation", "Xbox", "Nintendo", "Mercado Libre", "Google", "YouTube",
+        "Netflix", "Instagram", "TikTok", "Twitter (X)", "Discord", "Twitch"
+    ],
+    animales: [
+        "Perro", "Gato", "León", "Tigre", "Elefante", "Jirafa",
+        "Mono", "Carpincho", "Hornero", "Vaca", "Caballo", "Cerdo",
+        "Gallina", "Pato", "Águila", "Tiburón", "Ballena", "Delfín",
+        "Pingüino", "Oso Polar", "Canguro", "Koala", "Serpiente", "Araña",
+        "Mosquito", "Cucaracha", "Ratón", "Dinosaurio", "Dragón"
+    ],
+    lugares: [
+        "Escuela", "Hospital", "Comisaría", "Supermercado", "Kiosco",
+        "Plaza", "Playa", "Montaña", "Cine", "Shopping", "Gimnasio",
+        "Boliche", "Estadio de Fútbol", "Aeropuerto", "Baño", "Cocina",
+        "Cementerio", "Iglesia", "McDonalds", "Cárcel", "Zoologico"
     ]
 };
 
